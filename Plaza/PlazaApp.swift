53b2259b0ef4924f5efbd4b4e732902a4fa8fc4b
Felipe Alvarez <alv.araya@icloud.com>
2026-05-09 17:07:47 -0400

    Initial Commit

---
+++ Plaza/PlazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlazaApp.swift
+//  Plaza
+//
+//  Created by Felipe Álvarez on 09-05-26.
+//
+
+import SwiftUI
+
+@main
+struct PlazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
